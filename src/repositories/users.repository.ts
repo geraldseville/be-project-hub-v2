550debf4ac919af4be2aaa3f334907575c339466
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-07 22:11:13 +0800

    feat(user): add user repository

---
+++ src/repositories/users.repository.ts
@@ -0,0 +1,28 @@
+import { type Prisma } from '@prisma/client';
+
+import { prisma } from '../lib/prisma';
+
+export const usersRepository = {
+  deleteUser(id: string) {
+    return prisma.user.delete({
+      where: { id },
+    });
+  },
+
+  findUserById(id: string) {
+    return prisma.user.findUnique({
+      where: { id },
+    });
+  },
+
+  getAllUsers() {
+    return prisma.user.findMany();
+  },
+
+  updateUser(id: string, data: Prisma.UserUpdateInput) {
+    return prisma.user.update({
+      where: { id },
+      data,
+    });
+  },
+};
