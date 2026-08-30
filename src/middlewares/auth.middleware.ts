fbd585e91d8c2fc19a7b3bb62b957087ef950f39
Gerald <58556422+geraldseville@users.noreply.github.com>
2026-08-31 05:12:34 +0800

    refactor: explicitly declare .js extensions on import

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,4 +1,4 @@
-import { env } from '../config/env';
+import { env } from '../config/env.js';
 import jwt, { type JwtPayload } from 'jsonwebtoken';
 
 import { usersRepository } from '../repositories/user.repository.js';
